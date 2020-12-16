ec0320243aeb38fe425d6a681b0225dff05ebb57
Aco Basic <basic.aco@hotmail.com>
2020-12-16 02:02:24 +0100

    Pocetni meni

--- main.py
+++ main.py
@@ -1,16 +1,72 @@
-# This is a sample Python script.
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QStackedWidget, QPushButton, QDesktopWidget
+from PyQt5.QtGui import QIcon
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.centralWidget = QStackedWidget()
+        self.setCentralWidget(self.centralWidget)
+        self.mainMenuWidget = MainMenu()
+
+        self.menu()
+
+        self.setWindowTitle('Bubble Bobble')
+        self.setWindowIcon(QIcon('icon.png'))
+
+        self.show()
+
+    def center1(self):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move(int((screen.width() - size.width()) / 2), int((screen.height() - size.height()) / 2))
+
+    def menu(self):
+        self.mainMenuWidget.quitGameSignal.connect(self.quit)
+
+        self.centralWidget.addWidget(self.mainMenuWidget)
+        self.centralWidget.setCurrentWidget(self.mainMenuWidget)
+
+        self.resize(340, 280)
+        self.center1()
+
+    @staticmethod
+    def quit():
+        sys.exit()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('Cao')
+class MainMenu(QWidget):
 
-# Press the green button in the gutter to run the script.
+    quitGameSignal = pyqtSignal()
+
+    def __init__(self):
+        super(MainMenu, self).__init__()
+
+        button_width = 190
+        button_height = 50
+        button_left = 80
+        button_top = 70
+
+        self.play_button = QPushButton('Play', self)
+        self.play_button.setFixedWidth(button_width)
+        self.play_button.setFixedHeight(button_height)
+        self.play_button.move(button_left, button_top)
+
+        quit_button = QPushButton('Quit', self)
+        quit_button.setFixedWidth(button_width)
+        quit_button.setFixedHeight(button_height)
+        quit_button.move(button_left, button_top * 2)
+        quit_button.clicked.connect(self.quit)
+
+        self.show()
+
+    def quit(self):
+        self.quitGameSignal.emit()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
